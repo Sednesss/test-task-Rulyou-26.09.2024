34ca1c31a1788e613f82edb28f5d7528218ba241
PavelSedunov <pavel.sedunov@hoteza.com>
2024-09-26 14:22:52 +0700

    Adding Controllers for updating and deleting user

--- app/Http/Controllers/API/V1/User/DestroyController.php
+++ app/Http/Controllers/API/V1/User/DestroyController.php
@@ -3,14 +3,33 @@
 namespace App\Http\Controllers\API\V1\User;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class DestroyController extends Controller
 {
-    public function __invoke(Request $request): string
+    public function __invoke(Request $request, $id = null): JsonResponse
     {
-        dd($request->all());
-
-        return json_encode(123);
+        if ($id) {
+            $user = User::find($id);
+            if ($user) {
+                $user->delete();
+                return response()->json([
+                    'success' => true,
+                    'result' => $user
+                ]);
+            } else {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'User not found'
+                ], 404);
+            }
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'User not found'
+            ], 404);
+        }
     }
 }
--- app/Http/Controllers/API/V1/User/UpdateController.php
+++ app/Http/Controllers/API/V1/User/UpdateController.php
@@ -3,14 +3,55 @@
 namespace App\Http\Controllers\API\V1\User;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use Exception;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class UpdateController extends Controller
 {
-    public function __invoke(Request $request): string
+    public function __invoke(Request $request, $id): JsonResponse
     {
-        dd($request->all());
+        $statusCode = 500;
+        $responseBody = [
+            'success' => false,
+            'result' => 'Undefined error'
+        ];
 
-        return json_encode(123);
+        try {
+            $user = User::find($id);
+
+            if (!$user) {
+                return response()->json([
+                    'success' => false,
+                    'result' => 'User not found'
+                ], 404);
+            }
+
+            $payload = $request->only([
+                'full_name',
+                'role',
+                'efficiency'
+            ]);
+
+            $user->update($payload);
+
+            $statusCode = 200;
+            $responseBody = [
+                'success' => true,
+                'result' => [
+                    'id' => $user->id,
+                    'full_name' => $user->full_name,
+                    'role' => $user->role,
+                    'efficiency' => $user->efficiency
+                ]
+            ];
+        } catch (Exception $e) {
+            $responseBody['result'] = $e->getMessage();
+            Log::info('App::User::UpdateController: ' . $e->getMessage());
+        } finally {
+            return response()->json($responseBody, $statusCode);
+        }
     }
 }
